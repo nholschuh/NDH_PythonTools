56f2b6c9a8993c66a04d25043aa48b315975e8f2
Nick Holschuh <nick.holschuh@gmail.com>
2025-08-11 20:09:24 -0400

    created a cleaunup structure within data load to fix up the parameter object

--- radar_load.py
+++ radar_load.py
@@ -51,32 +51,25 @@ def radar_load(fn,plot_flag=0,elevation1_or_depth2=1,alternative_data_opt=0,trac
             radar_data = loadmat(fn_temp);
 
             ############### A helper function to clean up the parameter sections of spreadsheets
-            def struct_to_dict(x):+
-                def cv(o):
-                    if isinstance(o, np.void):                          # structured scalar
-                        return {n: cv(o[n]) for n in o.dtype.names}
-                    if isinstance(o, np.ndarray):
-                        if o.dtype.names:                               # structured array
-                            return cv(o[()]) if o.shape == () else [cv(r) for r in o]
-                        return cv(o.item()) if o.shape == () else [cv(e) for e in o]
-                    if isinstance(o, np.generic):                       # numpy scalar
-                        return o.item()
-                    return o
-                    
-                if isinstance(x,dict) == 0:
-                    return cv(x)
+            def structured_to_dict(arr):
+                """Recursively convert structured/nested numpy arrays to dicts."""
+                if isinstance(arr, np.ndarray) and arr.dtype.names:
+                    return {name: structured_to_dict(arr[name][0] if arr[name].shape == (1,) else arr[name]) 
+                            for name in arr.dtype.names}
+                elif isinstance(arr, np.ndarray) and arr.dtype == object:
+                    return [structured_to_dict(x) for x in arr]
                 else:
-                    return x
-
-            keylist = radar_data.keys()
-            param_cleanup = 0
-            for key in keylist:
-                if 'param' in key:
-                    if isinstance(radar_data[key],dict) == 0:
-                        radar_data[key] = struct_to_dict(radar_data[key])
-                        param_cleanup = 1
-            if param_cleanup == 1:
-                print('Fixed parameter structure')
+                    return arr.tolist() if isinstance(arr, np.ndarray) else arr
+            
+                keylist = radar_data.keys()
+                param_cleanup = 0
+                for key in keylist:
+                    if 'param' in key:
+                        if isinstance(radar_data[key],dict) == 0:
+                            radar_data[key] = struct_to_dict(radar_data[key])
+                            param_cleanup = 1
+                if param_cleanup == 1:
+                    print('Fixed parameter structure')
                     
             
 
